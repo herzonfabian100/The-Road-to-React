e433ff03c41cfe818bdd65838a906018dca33cb6
Hz Fabian Aparicio <44126998+herzonfabian100@users.noreply.github.com>
2022-01-25 19:28:02 -0800

    Initialize 2021/react-testing-setup

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+describe('something truthy and falsy', () => {
+  test('true to be true', () => {
+    expect(true).toBeTruthy();
+  });
+
+  test('false to be false', () => {
+    expect(false).toBeFalsy();
+  });
+});
